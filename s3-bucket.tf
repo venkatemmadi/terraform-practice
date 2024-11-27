d59587a4af264b190ca1ff59d48a238d413f5a10
venkatemmadi <venkat.venky72@gmail.com>
2024-11-27 22:28:53 +0530

    creating  s3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,8 @@
+# This below code creates S3 bucket  using terraform template
+
+provider "aws" {
+  region = "us-west-1"
+}
+resource "aws_s3_bucket" "my_bucket" {
+  bucket  = "s3-bucket creation"
+}
